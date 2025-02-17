53dba88c1ad89ce51ecbe880ec705e03e8beea1f
Sonja Rogner <rog230113@spengergasse.at>
2025-02-17 12:47:04 +0100

    ..

---
+++ 2025-02-11/script.js
@@ -0,0 +1,33 @@
+const ul = document.querySelector("ul");
+
+const counterElement = document.querySelector("#counter");
+counterElement.addEventListener("click", generateList);
+
+const deleteElement = document.querySelector("#delete");
+deleteElement.addEventListener("click", deleteList);
+
+let x = 10;
+const increase = document.querySelector("#plus")
+increase.onClick = function () {
+  x++;
+};
+
+const minus = document.querySelector("#minus")
+
+function generateList() {
+  for (let i = 1; i <= x; i++) {
+    const li = document.createElement("li");
+    li.innerText = i;
+    ul.appendChild(li);
+  }
+}
+
+function deleteList() {
+  const liRemove = ul.lastElementChild;
+  if (liRemove) {
+    ul.removeChild(liRemove);
+  } else {
+    console.log("Hey, es gibt keine Listenelemente mehr!!!");
+    alert("Hey, es gibt keine Listenelemente mehr!!!")
+  }
+}
