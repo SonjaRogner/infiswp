39175a41114b0c55b9a277e4936c3600766f2c19
Sonja Rogner <rog230113@spengergasse.at>
2025-03-02 23:36:10 +0100

    ..

--- 2025-02-25a/static/script.js
+++ 2025-02-25a/static/script.js
@@ -6,28 +6,42 @@ counterElement.addEventListener("click", generateList);
 const deleteElement = document.querySelector("#delete");
 deleteElement.addEventListener("click", deleteList);
 
-let x = 10;
-const increase = document.querySelector("#plus")
-increase.onClick = function () {
-  x++;
-};
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
 
-const minus = document.querySelector("#minus")
+let x = 10;
+
+plus.addEventListener("click", () => {
+  x++;
+  generateList();
+});
+
+minus.addEventListener("click", () => {
+  if (x > 0) {
+    x--; 
+    const liRemove = ul.lastElementChild;
+    if (liRemove) {
+      ul.removeChild(liRemove);
+    }
+  } else if (x < 1) {
+    console.log("Hey, es gibt keine Listenelemente mehr!!!");
+    alert("Hey, es gibt keine Listenelemente mehr!!!")
+  }
+
+});
 
 function generateList() {
+  ul.innerHTML = "";
   for (let i = 1; i <= x; i++) {
     const li = document.createElement("li");
     li.innerText = i;
-    ul.appendChild(li);
+    li.appendChild(li);
+    if (i % 2 === 0) {
+      ul.classList.add("gerade");
+    }
   }
 }
 
 function deleteList() {
-  const liRemove = ul.lastElementChild;
-  if (liRemove) {
-    ul.removeChild(liRemove);
-  } else {
-    console.log("Hey, es gibt keine Listenelemente mehr!!!");
-    alert("Hey, es gibt keine Listenelemente mehr!!!")
-  }
+  ul.innerHTML = "";
 }
